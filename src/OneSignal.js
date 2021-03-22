5d31bde164a430ff041bf15a2eb8c792f7e76df3
Ghaffaru <mudashiruagm@gmail.com>
2021-03-22 15:28:02 +0000

    feat: send notification to one and all devices

--- src/OneSignal.js
+++ src/OneSignal.js
@@ -25,7 +25,7 @@ class OneSignal {
         }
     }
 
-    async allDevices (fields) {
+    async allDevices () {
         try {
             const endpoint = `https://onesignal.com/api/v1/players?app_id=${this.appId}&limit=300&offset=0`;
 
@@ -36,9 +36,58 @@ class OneSignal {
             });
 
             return response.data
-            
+
         } catch (err) {
-            throw Error(err.response.data.errors)
+            throw Error(err.response.data.errors);
+        }
+    }
+
+    async sendNotificationTo (deviceToken, fields) {
+        try {
+            const devices = await this.allDevices();
+    
+            const user = devices.players.find(player => player.identifier === deviceToken);
+
+            const endpoint = 'https://onesignal.com/api/v1/notifications';
+
+            fields['app_id'] = this.appId
+            fields['include_player_ids'] = [user.id]
+
+            const response = await axios.post(endpoint, fields, {
+                headers: {
+                    Authorization: `Basic ${this.restApiKey}`
+                }
+            });
+           
+            return response.data
+
+        } catch (err) {
+            throw Error(err.response.data.errors);
+        }
+    }
+
+    async sendNotificationToAll(fields) {
+        try {
+            const devices = await this.allDevices();
+
+            const endpoint = 'https://onesignal.com/api/v1/notifications';
+
+            const playerIds = [];
+            devices.players.forEach(player => {
+                playerIds.push(player.id)
+            });
+            fields['include_player_ids'] = playerIds;
+            fields['app_id'] = this.appId
+
+            const response = await axios.post(endpoint, fields, {
+                headers: {
+                    Authorization: `Basic ${this.restApiKey}`
+                }
+            });
+
+            return response.data
+        } catch (err) {
+            throw Error(err.response.data.errors);
         }
     }
 }
--- test/index.js
+++ test/index.js
@@ -30,9 +30,52 @@ describe('One signal tests', () => {
             const getAllDevicesResponse = await client.allDevices();
 
             chai.expect(getAllDevicesResponse).to.have.property('players');
+
+        } catch (err) {
+            throw Error(err.message);
+        }
+    });
+
+    it ('can send notification to a user', async () => {
+        try {
+            fields = {
+              
+                "headings":{
+                    "en":  "The African Wear",
+                },
+                "contents": {
+                    "en": "Hello Notification"
+                },
+                "web_url": "https://blahblab.com/",
+                "mobile_url": "/chat"
+            }
+            const sendNotificationResponse = await client.sendNotificationTo('ce777617da7f548fe7a9ab6febb56cf39fba6d382000c0395666288d961ee566', fields);
+            
+            chai.expect(sendNotificationResponse).to.have.property('id');
+
+        } catch (err) {
+            throw new Error(err.message);
+        }
+    });
+
+    it('can send notification to all users', async () => {
+        try {
+            fields = {
+                "headings":{
+                    "en":  "The African Wear Customer",
+                },
+                "contents": {
+                    "en": "Hello Notification"
+                },
+                "web_url": "https://blahblab.com/",
+                "mobile_url": "/chat"
+            }
+            const sendNotificationResponse = await client.sendNotificationToAll(fields);
+            
+            chai.expect(sendNotificationResponse).to.have.property('id');
             
         } catch (err) {
-
+            throw new Error(err.message);
         }
     })
 });
